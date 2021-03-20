9e9f41cf681e8ed689235259376ebb2d898b7aea
Hazork <arthur.fiorette@gmail.com>
2021-03-20 16:05:57 -0300

    Pipeline returns his context

--- app/src/util/middleware.ts
+++ app/src/util/middleware.ts
@@ -1,10 +1,8 @@
-
 export type NextFunction = () => Promise<void> | void;
 
 export type Middleware<T> = (context: T, next: NextFunction) => void;
 
 export default class Pipeline<T> {
-
   constructor(private middlewares: Middleware<T>[] = []) {}
 
   use(...middlewares: Middleware<T>[]) {
@@ -16,13 +14,18 @@ export default class Pipeline<T> {
   }
 
   async execute(context: T) {
-    await this.run(context, [...this.middlewares]);
+    return await this.run(context, [...this.middlewares]);
   }
 
   private async run(context: T, middlewares: Middleware<T>[]) {
     if (middlewares.length !== 0) {
       const next = middlewares.shift();
-      next && await next(context, async () => await this.run(context, middlewares));
+      if (next) {
+        await next(context, () => {
+          this.run(context, middlewares);
+        });
+      }
     }
+    return context;
   }
 }
