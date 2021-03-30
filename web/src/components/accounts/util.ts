4d84f3b20514744fe1c2b0a707dc682ffa0a960e
Hazork <arthur.fiorette@gmail.com>
2021-03-30 18:58:52 -0300

    Create action and edit todos

--- web/src/components/accounts/util.ts
+++ web/src/components/accounts/util.ts
@@ -1,4 +1,6 @@
 // TODO: Fetch user profile photo correctly
+//
+// Probably only possible when Steam WEB API is successfully implemented
 export function fetchSteamUserImage() {
   return 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/b5/b5bd56c1aa4644a474a2e4972be27ef9e82e517e_full.jpg'; // Default steam profile photo
 }
