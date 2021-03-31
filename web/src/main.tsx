714962d95ca686e47efbacb7907d023346e13ec5
Hazork <arthur.fiorette@gmail.com>
2021-03-31 12:37:02 -0300

    Background and vertical sizing adjustments

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,37 +2,29 @@ import React from 'react';
 
 import InfoBox from './components/infoBox';
 import Logs from './components/logs';
-import Account from './components/accounts';
+import Accounts from './components/accounts';
 import Trades from './components/trades';
 
 export default function Main() {
   return (
     <main id="main">
-      <div className="container h-100 pb-5">
-        <Row>
-          <Column size="12 col-xl-7 col-xxl-8" title="Trades">
-            <Trades />
-          </Column>
-          <Column size="12 col-md-6 col-xl-5 col-xxl-4" title="Accounts">
-            <Account />
-          </Column>
-          <Column size="12 col-md-6 col-xl-12" title="Logs">
-            <Logs />
-          </Column>
-        </Row>
+      <div className="container mb-3">
+        <div className="row">
+          <InfoBoxColumn size="xl-7 col-xxl-8" title="Trades" Children={Trades} />
+          <InfoBoxColumn size="md-6 col-xl-5 col-xxl-4" title="Accounts" Children={Accounts} />
+          <InfoBoxColumn size="md-6 col-xl-12" title="Logs" Children={Logs} />
+        </div>
       </div>
     </main>
   );
 }
 
-function Column({ size, title, children }: any) {
+function InfoBoxColumn({ size, title, Children }: any) {
   return (
-    <section className={`col-${size} mt-3`}>
-      <InfoBox title={title}>{children}</InfoBox>
+    <section className={`col-12 col-${size} mt-3`}>
+      <InfoBox title={title}>
+        <Children />
+      </InfoBox>
     </section>
   );
-}
-
-function Row({ children }: any) {
-  return <div className="row mb-4">{children}</div>;
 }
